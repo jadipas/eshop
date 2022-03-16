a7af57411f92f266bd60228c4ba76bd256149de2
Jordan <50451501+Chibuske@users.noreply.github.com>
2022-03-16 21:29:57 +0200

    a

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import React from "react";
 import Header from "./Header.js";
+import Home from "./Home.js";
 
 function App() {
   return (
     <div className="app">
-      <Header />
-      {/* Home */}
+      < Header />
+      < Home />
     </div>
   );
 }
