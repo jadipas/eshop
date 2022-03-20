ea1348eeb4e87a75f00132397260b6f5b960c9b7
Jordan <50451501+Chibuske@users.noreply.github.com>
2022-03-20 14:19:30 +0200

    state provider

--- src/StateProvider.js
+++ src/StateProvider.js
@@ -5,10 +5,10 @@ export const StateContext = createContext();
 
 //wrap our app and provide the DataLayer to every Component
 export const StateProvider = ({ reducer, initialState, children }) => (
-    <StateContext.Provider value={userReducer(reducer, initialState)}>
+    <StateContext.Provider value={useReducer(reducer, initialState)}>
         {children}
     </StateContext.Provider>
 )
 
 //pour information from the DataLayer
-export const useStateValue = () => useContext(stateContext);
+export const useStateValue = () => useContext(StateContext);
--- src/Subtotal.js
+++ src/Subtotal.js
@@ -1,9 +1,9 @@
-import { SportsBasketball } from '@mui/icons-material'
 import React from 'react'
 import CurrencyFormat from 'react-currency-format'
 import "./Subtotal.css"
 
 function Subtotal() {
+
   return (
     <div className='subtotal'>
         <CurrencyFormat
