40b506b35fea4240065fdafbd0f9c7b35049807e
Jordan <50451501+Chibuske@users.noreply.github.com>
2022-03-20 14:33:07 +0200

    subtotal count + total price

--- src/Product.js
+++ src/Product.js
@@ -6,8 +6,6 @@ function Product({id, title, image, price, rating}) {
 
   const [{ basket }, dispatch] = useStateValue();
 
-  console.log('this is the basket >>>', basket)
-
   const addToBasket = () => {
       // dispatch item to data layer
       dispatch({
