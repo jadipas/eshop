0cd45d193d7ebea0b5227b7e90faf2c380b38f79
Jordan <50451501+Chibuske@users.noreply.github.com>
2022-04-26 12:00:19 +0300

    footer css

--- web/src/Footer.js
+++ web/src/Footer.js
@@ -2,19 +2,27 @@ import React from 'react'
 import "./Footer.css"
 import InstagramIcon from '@mui/icons-material/Instagram';
 import { Link } from 'react-router-dom';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TwitterIcon from '@mui/icons-material/Twitter';
 
 function Footer() {
   return (
     <div className='footer'>
-      <div>
+      <div className='footer__description'>
         <h2>FWASH</h2>
-        <p>The Fast Wash</p>
+        <p>The Fastest Wash</p>
       </div>
 
-      <div>
+      <div className='footer__socials'>
         <h3>Socials</h3>
         <Link to='/'>
+          <FacebookIcon />
+        </Link>
+        <Link to='/'>
           <InstagramIcon />
+        </Link>
+        <Link to='/'>
+          <TwitterIcon />
         </Link>
       </div>
     </div>
