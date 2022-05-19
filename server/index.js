fb6dca5b94e842f9c458fdc3a00a0838f23cc0a4
jadipas <50451501+jadipas@users.noreply.github.com>
2022-05-19 12:34:18 +0300

    started db setup

--- server/index.js
+++ server/index.js
@@ -1,49 +1,52 @@
-const express = require('express')
-const app = express()
-require('dotenv').config()
-const stripe = require('stripe')(process.env.STRIPE_SECRET_TEST)
-const bodyParser = require('body-parser')
-const cors = require('cors')
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const stripe = require("stripe")(process.env.STRIPE_SECRET_TEST);
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
-app.use(bodyParser.urlencoded({ extended: true}))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-app.use(cors())
+app.use(cors());
 
-const mysql = require('mysql')
+const mysql = require("mysql");
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'dbuser',
-  password: 's3kreee7',
-  database: 'my_db'
-})
+  host: "localhost",
+  user: process.env.SQL_USER,
+  password: process.env.SQL_PASSWORD,
+  database: "db",
+});
 
-connection.connect()
+connection.connect();
 
 app.post("/payments/create", async (request, response) => {
-    const total = request.query.total;
-    console.log('Payment Request Received for ', total);
+  const total = request.query.total;
+  console.log("Payment Request Received for ", total);
 
-    const paymentIntent = await stripe.paymentIntents.create({
-        amount: total,
-        currency: "USD",
-    })
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: total,
+    currency: "USD",
+  });
 
-    //OK - Created
-    response.status(201).send({
-        clientSecret: paymentIntent.client_secret,
-    })
-})
+  //OK - Created
+  response.status(201).send({
+    clientSecret: paymentIntent.client_secret,
+  });
+});
 
 app.post("/product_page", async (request, response) => {
-    const id = request.query.id;
-    console.log('Product id >> ', id);
+  const id = request.query.id;
+  console.log("Product id >> ", id);
+  connection.query("SELECT * FROM  product", (err, result, fields) => {
+    if (err) throw err;
 
+    console.log("The solution is: ", result);
+  });
+});
 
-})
+app.listen(process.env.PORT || 4000, () => {
+  console.log("Server is listening on port 4000");
+});
 
-app.listen(process.env.PORT || 4000, ()=> {
-    console.log("Server is listening on port 4000")
-})
-
-connection.end()
+//connection.end();
--- web/src/ProductPage.js
+++ web/src/ProductPage.js
@@ -1,20 +1,29 @@
-import React, { useEffect } from 'react'
-import { useSearchParams } from 'react-router-dom';
-import "./ProductPage.css"
+import React, { useEffect } from "react";
+import { useSearchParams } from "react-router-dom";
+import axios from "./axios";
+import "./ProductPage.css";
 
 function ProductPage() {
   const [searchParams] = useSearchParams();
-  console.log(searchParams.get('sort')); // 'name'
 
   useEffect(() => {
-        
-    }, [])
+    const getProductInfo = async () => {
+      try{
+        const response = await axios({
+          method: "post",
+          //Stripe expects the total in a currencies subunits
+          url: `/product_page?id=${searchParams.get("id")}`,
+        });
+      }catch(error){
+        console.log(error)
+      }
+      //getProductInfo(response.data.clientSecret);
+    };
 
-  return (
-    <div className='productPage'>
-        ProductPage
-    </div>
-  )
+    getProductInfo();
+  }, []);
+
+  return <div className="productPage">ProductPage</div>;
 }
 
-export default ProductPage
+export default ProductPage;
