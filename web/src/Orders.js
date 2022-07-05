bb50176b94e9d585fa4fac928edcf3bb67bcd5b4
jadipas <50451501+jadipas@users.noreply.github.com>
2022-07-05 12:04:16 +0300

    orders page done

--- web/src/Orders.js
+++ web/src/Orders.js
@@ -1,18 +1,17 @@
 import axios from "./axios";
 import React, { useEffect, useState } from "react";
-import { useSearchParams } from "react-router-dom";
 import { useStateValue } from "./StateProvider";
+import { Link } from "react-router-dom";
 import "./Orders.css";
 
 function Orders() {
-  const [searchParams] = useSearchParams();
-  const [info, setInfo] = useState();
+  const [info, setInfo] = useState([]);
   const [loading, setLoading] = useState(true);
   const [{ basket, user }, dispatch] = useStateValue();
 
   useEffect(() => {
     const getProductInfo = async () => {
-      console.log(user)
+      console.log(user);
       await axios
         .get("/orders", {
           headers: {
@@ -20,9 +19,11 @@ function Orders() {
           },
         })
         .then(function (response) {
-          setInfo(response.data);
-          //console.log(response.data);
+          setInfo(response.data.orders);
+          console.log(response.data.orders);
+          //console.log(info);
           setLoading(false);
+          return response.data.orders;
         })
         .catch(function (error) {
           console.log(error);
@@ -30,12 +31,39 @@ function Orders() {
     };
 
     getProductInfo();
+    console.log(info);
   }, []);
 
   if (loading) {
     return <div className="productPage">Loading...</div>;
   } else {
-    return null
+    return (
+      <div className="orders">
+        {info.map((order, index) => {
+          return (
+            <div key={index} className="orders__row">
+              <h3>{order.date}</h3>
+              <div className="orders__rowTitles">
+                <h4>Product ID</h4>
+                <h4>Name</h4>
+                <h4>Amount</h4>
+              </div>
+              {order.products.map((product, id) => {
+                return (
+                  <div key={id} className="orders__rowContent">
+                    <p>{product.id}</p>
+                    <p>
+                      <Link to={"/product_page?id=" + product.id}>{product.name}</Link>
+                    </p>
+                    <p>{product.amount}</p>
+                  </div>
+                );
+              })}
+            </div>
+          );
+        })}
+      </div>
+    );
   }
 }
 
